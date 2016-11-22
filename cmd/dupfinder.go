8da4daa8aa394a51695e86102d13d9d7a7532eea
Chris Erwin <crerwin@gmail.com>
2016-11-22 11:07:39 -0500

    more reorganization

--- cmd/dupfinder.go
+++ cmd/dupfinder.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"crypto/sha256"
 	"flag"
-	"fmt"
-	"log"
 	"os"
-	"path/filepath"
 
 	"github.com/crerwin/dupfinder/duptools"
 )
-
-var filesByHash = make(map[[sha256.Size]byte]string)
 
 func main() {
 	if len(os.Args) != 2 {
@@ -19,36 +13,6 @@ func main() {
 	} else {
 		flag.Parse()
 		path := flag.Arg(0)
-		fmt.Println(len(os.Args), os.Args)
-		inventoryFilesByName(path)
-	}
-}
-
-func fileVisited(path string, f os.FileInfo, err error) error {
-	if err != nil {
-		log.Print(err)
-		return nil
-	}
-	if f.IsDir() {
-		return nil
-	}
-	fmt.Printf("%s calculating hash: ", path)
-	hash, _ := duptools.GetHash(path)
-	fmt.Printf("%x\n", hash)
-	var hashArray [sha256.Size]byte
-	copy(hashArray[:], hash)
-	if p, ok := filesByHash[hashArray]; ok {
-		fmt.Printf("%q is a duplicate of %q\n", path, p)
-	} else {
-		filesByHash[hashArray] = path
-	}
-	return nil
-}
-
-func inventoryFilesByName(path string) {
-	fmt.Println("Inventorying files in " + path)
-	err := filepath.Walk(path, fileVisited)
-	if err != nil {
-		log.Fatal(err)
+		duptools.FindDups(path)
 	}
 }
--- duptools/duptools.go
+++ duptools/duptools.go
@@ -2,11 +2,49 @@ package duptools
 
 import (
 	"crypto/sha256"
+	"fmt"
 	"io"
+	"log"
 	"os"
+	"path/filepath"
 )
 
-func GetHash(filePath string) ([]byte, error) {
+var filesByHash = make(map[[sha256.Size]byte]string)
+
+func FindDups(path string) {
+	inventoryFilesByName(path)
+}
+
+func inventoryFilesByName(path string) {
+	fmt.Println("Inventorying files in " + path)
+	err := filepath.Walk(path, fileVisited)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func fileVisited(path string, f os.FileInfo, err error) error {
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+	if f.IsDir() {
+		return nil
+	}
+	fmt.Printf("%s calculating hash: ", path)
+	hash, _ := getHash(path)
+	fmt.Printf("%x\n", hash)
+	var hashArray [sha256.Size]byte
+	copy(hashArray[:], hash)
+	if p, ok := filesByHash[hashArray]; ok {
+		fmt.Printf("%q is a duplicate of %q\n", path, p)
+	} else {
+		filesByHash[hashArray] = path
+	}
+	return nil
+}
+
+func getHash(filePath string) ([]byte, error) {
 	var result []byte
 	file, err := os.Open(filePath)
 	if err != nil {
